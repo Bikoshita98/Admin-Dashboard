b1f853ecf57080b5e781a28f2efc05e54adc7370
Bikoshita Gogoi <bikoshitagogoi@Bikoshitas-MacBook-Pro.local>
2026-02-25 08:55:52 +0530

    VC-123 added signals

--- src/app/features/auth/auth.store.ts
+++ src/app/features/auth/auth.store.ts
@@ -3,5 +3,18 @@ import { User } from '../../core/models/user.model';
 
 class AuthStore {
     private _user = signal<User | null>(null);
-    
+
+    user = computed(() => this._user());
+    isAuthenticated = computed(() => !!this._user());
+    role = computed(() => this._user()?.role);
+
+    setUser(user: User | null) {
+        this._user.set(user);
+    }
+
+    clear(){
+        this._user.set(null);
+    }
 }
+
+export const authStore = new AuthStore();
